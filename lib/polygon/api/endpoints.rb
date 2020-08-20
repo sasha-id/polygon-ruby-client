5255b4848af46ffef79c696a9fe13c9a96313bcd
Alex Yusupov <alex@klepa.co>
2020-08-20 13:05:17 -0400

    Added aggregates endpoint

--- lib/polygon/api/endpoints.rb
+++ lib/polygon/api/endpoints.rb
@@ -3,8 +3,12 @@
 module Polygon
   module Api
     module Endpoints
-      def trades(symbol, date, options = {})
+      def trades(symbol:, date:, options: {})
         get("/v2/ticks/stocks/trades/#{symbol}/#{date}", { apiKey: api_key }.merge(options))
+      end
+
+      def aggregates(symbol:, multiplier:, timespan:, from:, to:, options: {})
+        get("/v2/aggs/ticker/#{symbol}/range/#{multiplier}/#{timespan}/#{from}/#{to}", { apiKey: api_key }.merge(options))
       end
     end
   end
